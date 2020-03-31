bfa6439a88f93029170e3c6b5372e5a5b803d7fb
Unknown <verner@fortelius.net>
2020-03-31 09:30:17 +0300

    fix column output

--- jsontestapp/Controllers/JsontestController.cs
+++ jsontestapp/Controllers/JsontestController.cs
@@ -71,7 +71,7 @@ namespace jsontestapp.Controllers
 
                 return Ok(new
                 {
-                    orderBy = orderBy,
+                    orderBy = column,
                     executionMilliseconds = stopwatch.ElapsedMilliseconds,
                     items = result,
                 });
@@ -110,6 +110,41 @@ namespace jsontestapp.Controllers
             // otherwise the first part is the column, and the rest is the json path
             var jsonPathParameter = "$." + string.Join(".", parts.Skip(1).Select(o => $@"""{o}"""));
             return ($"JSON_VALUE({column}, @jsonPath)", jsonPathParameter);
+        }
+
+
+        /// <summary>
+        /// Convert a path to either a column or proper json_value column and path
+        /// Validates the column against a model passed in as T
+        /// LocalDb and previous versions of SQL server do not support json paths as parameters
+        /// </summary>
+        /// <param name="input">column[/jsonpath]</param>
+        /// <returns></returns>
+        private (string column, string jsonPathParameter) ToPathOrColumnPre2017<T>(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentException("Column cannot be null or empty");
+            }
+
+            var parts = input.Split("/");
+            var column = parts[0];
+
+            var properties = typeof(T).GetProperties().Select(o => o.Name.ToUpperInvariant());
+            if (!properties.Contains(column.ToUpperInvariant()))
+            {
+                throw new ArgumentException($"Input does not contain a valid column name: {input}");
+            }
+
+            // if the input only contains one part it is a real column
+            if (parts.Length == 1)
+            {
+                return (column, null);
+            }
+
+            // otherwise the first part is the column, and the rest is the json path
+            var jsonPathParameter = "$." + string.Join(".", parts.Skip(1).Select(o => $@"""{o}"""));
+            return ($"JSON_VALUE({column}, '{jsonPathParameter}')", null);
         }
     }
 }
